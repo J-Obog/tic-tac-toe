310041e007b1b51f15f720b94a4a6753e494cae5
JObog <jobogbaimhe@gmail.com>
2021-10-01 18:37:50 -0400

    Created function to print out board
    Created function to print out board

--- game.py
+++ game.py
@@ -1,11 +1,15 @@
 print("Hello player!") 
 
-board = [['-', '-', '-'],
+def printBoard(board):
+    for i in range(3):
+        print(board[i][0], board[i][1], board[i][2])
+
+def main():
+    board = [['-', '-', '-'],
          ['-', '-', '-'],
          ['-', '-', '-']]
 
-def main():
-  pass
+    printBoard(board)
 
-if __name__=="main":
+if __name__=="__main__":
   main()
