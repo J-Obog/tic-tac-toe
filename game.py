287188950ddd6e5fcac4b46be93c32c8d4974f16
JObog <jobogbaimhe@gmail.com>
2021-10-01 19:08:22 -0400

    Added basic user input system

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
 print("Hello player!") 
 
 def printBoard(board):
-    for i in range(3):
-        print(board[i][0], board[i][1], board[i][2])
+    for i in range(0,9,3):
+        print(board[i], board[i+1], board[i+2])
 
 def main():
-    board = [['-', '-', '-'],
-         ['-', '-', '-'],
-         ['-', '-', '-']]
+    board = ['-', '-', '-', '-', '-', '-', '-', '-', '-']
     
-    playerTurn = 0
-    playing = False
+    playerTurn = 1
+    playing = True
 
     while(playing):
-        pass
+        print(f"Player {playerTurn} turn!")
+        printBoard(board)
+        spot = int(input("Please pick a spot (1-9)"))
+        board[spot - 1] = 'X' if playerTurn == 1 else 'O'
+        playerTurn = 2 if playerTurn == 1 else 1
+        break
 
 if __name__=="__main__":
   main()
